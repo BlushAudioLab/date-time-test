3bcdadbe20a35dd6e5f91c8622ecb446de8ee4c7
Blush Audio Dev <blushaudio@gmail.com>
2021-12-23 20:04:50 +0000

    DateTimePicker Imported
    Sort of working, sort of not. Pretty shite.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
+import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+// import { NgxMatMomentModule } from '@angular-material-components/moment-adapter';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +23,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    NgxMatDatetimePickerModule,
+    NgxMatTimepickerModule,
+    NgxMatNativeDateModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatMomentDateModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
